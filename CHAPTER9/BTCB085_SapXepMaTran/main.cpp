c5d6cc4f31d6550c013b532663c39c0d63857d78
LPToan <lephuoctoan6411@gmail.com>
2025-09-06 23:10:47 +0700

    update

--- CHAPTER9/BTCB085_SapXepMaTran/main.cpp
+++ CHAPTER9/BTCB085_SapXepMaTran/main.cpp
@@ -1,6 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+double m[105*105];
+void nhap(int &n, int &m){
+    n = 0;
+    string temp;
+    while(getline(cin, temp)){
+        m = 0;
+        double so;
+        stringstream s (temp);
+        while (s>>so){
+            mt[n][m] = so;
+            m++;
+        }
+        n++;
+    }
+}
+}
+void mttangtheodong(int n, int m){
+    cout << "Cau f: ma tran tang theo dong\n";
+    for(int d = 0; i<n; ++d){
+}
 int main()
 {
 

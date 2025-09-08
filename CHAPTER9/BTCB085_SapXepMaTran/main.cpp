1e1ba3377d2b53e866731cb34e53a25320b7fe57
LPToan <lephuoctoan6411@gmail.com>
2025-09-08 23:11:52 +0700

    update

--- CHAPTER9/BTCB085_SapXepMaTran/main.cpp
+++ CHAPTER9/BTCB085_SapXepMaTran/main.cpp
@@ -16,23 +16,24 @@ void nhap(int &m, int &n){
     }
 }
 void mtma(int m, int n, int &f){
+    f=0;
     for(int i = 0; i<n; ++i){
-        for(int k = 0; k<m; ++k{
+        for(int k = 0; k<m; ++k){
             ma[f] = mt[i][k];
             f++;
         }
     }
 }
 void xuatma(int m, int f){
-    for(int i = 1; i<f; ++i){
-        if(i%m==0) cout << ma[i] << "\n";
+    for(int i = 0; i<f; ++i){
+        if((i+1)%m==0) cout << ma[i] << "\n";
         else cout << ma[i] << " ";
     }
 }
 void xuatmt(int m, int n){
     for(int i = 0; i<n; ++i){
         for(int k = 0; k<m; ++k){
-            cout << mt[n][m] << " ";
+            cout << mt[i][k] << " ";
         }
         cout << "\n";
     }
@@ -40,12 +41,51 @@ void xuatmt(int m, int n){
 void mttangtheodong(int m, int f){
     cout << "Cau f: ma tran tang theo dong\n";
     sort(ma, ma + f);
-    xuat(m,f);
+    xuatma(m,f);
 }
-void mtgiamtheocot(int m, int n){
+void mtgiamtheocot(int m, int n, int f){
     cout << "Cau g: ma tran giam theo cot\n";
-    for(int i = 0; i<n; ++i){
-
+    sort(ma,ma+f, greater<double>());
+    f=0;
+    for(int i = 0; i<m; ++i)
+        for(int j = 0; j<n; ++j){
+            mt[j][i] = ma[f];
+            f++;
+        }
+    xuatmt(m,n);
+}
+void mttangdantheoduongcheochinh(int m, int n, int f){
+    if(m!=n) cout << "No";
+    else{
+        sort(ma,ma+f);
+        cout << "Cau h: ma tran tang theo duong cheo chinh\n";
+        int ld = n-1, lc = 0, i = 0, vtd = n-1, vtc=0;
+        while(i<n*m){
+            mt[vtd][vtc] = ma[i];
+            vtd++; vtc++; i++;
+            if(vtd>=n){
+                vtd = ld-1;
+                vtc=0;
+            }
+            if(vtc>=m){
+                vtc = lc+1;
+                vtd=0;
+            }
+        }
+        ld = 0, lc = 1, i = 0, vtd = 0, vtc=1;
+        while(i<n*m){
+            mt[vtd][vtc] = ma[i];
+            vtd++; vtc++; i++;
+            if(vtd>=n){
+                vtd = ld-1;
+                vtc=0;
+            }
+            if(vtc>=m){
+                vtc = lc+1;
+                vtd=0;
+            }
+        }
+        xuatmt(m,n);
     }
 }
 int main()
@@ -54,11 +94,13 @@ int main()
     freopen("BTCB085.INP", "r", stdin);
     cin.tie(0); cout.tie(0);
     freopen("BTCB085.OUT", "w", stdout);
-    int m,f;
-    nhap(m,f);
+    int m,f,n;
+    nhap(m,n);
     mtma(m,n,f);
     mttangtheodong(m,f);
     cout << "\n" << "\n";
-
+    mtgiamtheocot(m,n,f);
+    cout << "\n" << "\n";
+    mttangdantheoduongcheochinh(m,n,f);
     return 0;
 }
